d1c9b6e08e4bb4eab211239d8d88eb63a2a4d135
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2026-02-01 21:57:38 +0530

    created custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button"
+import Multistepform from "./components/multistepform"
 const App = () => {
   return (
     <div>
-    <Button >Click me</Button>
-    <Button variant="outline">Button</Button>
+   <Multistepform/>
     </div>
   )
 }
---
+++ src/components/Multistepform.tsx
@@ -0,0 +1,15 @@
+// import { useForm } from "react-hook-form";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { z } from "zod";
+// import type { StepFormData } from "@/types";
+const Multistepform = () => {
+
+  // const {register,handleSubmit, formState:{errors}, trigger,setValue,reset }= useForm<StepFormData>({
+  //   // resolver:zodResolver(getCurrentStepSchema()),
+  // })
+  return (
+    <div>multi-step-form</div>
+  )
+}
+
+export default Multistepform
